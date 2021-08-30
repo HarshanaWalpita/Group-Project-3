2fda410aea21b446e1e5710984b6126651c9e907
HarshanaWalpita <harshanawalpita@gmail.com>
2021-08-31 02:13:47 +0530

    errors

--- server.js
+++ server.js
@@ -25,16 +25,18 @@ connectDB();
 app.use(express.json());
 
 
-if (process.env.NODE_ENV === "production"){
-app.use(express.static(path.join(__dirname, "/client/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
 
-app.get("*", (req,res)=>{
-res.sendFile(path.join(__dirname,"client", "build", "index.html"));
-});
-}else{
-app.get("/", (req,res) => {
-res.send("Api running");
-});
+else {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
 }
 
 // Connecting Routes
