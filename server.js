3e62816705f304c0cfd0f993fc7603d15ca5e1b6
HarshanaWalpita <harshanawalpita@gmail.com>
2021-08-31 08:19:38 +0530

    again

--- server.js
+++ server.js
@@ -25,19 +25,12 @@ connectDB();
 app.use(express.json());
 
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
-}
-
-else {
-  app.use(express.static(path.join(__dirname, '/client/public')));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  let url = path.join(__dirname, '../client/build', 'index.html');
+  if (!url.startsWith('/app/')) // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
+});
 
 // Connecting Routes
 app.use("/api/auth", require("./routes/auth"));
